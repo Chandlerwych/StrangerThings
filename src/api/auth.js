94e17e75cec964568c0a378dac23b719fd5b2855
Brandon Norris <bsn2qg@virginia.edu>
2022-12-07 11:22:14 -0500

    Changed to getPosts API

--- src/api/auth.js
+++ src/api/auth.js
@@ -7,13 +7,21 @@ export const getPosts = async (setPosts) => {
   // take the returned data and parse into JSON 
   // return the results (array of objects) to be displayed on the poage via  our Posts component 
 
-  try {
-    const response = await fetch (`https://strangers-things.herokuapp.com/api/${cohortName}/posts`);
-    const {data: {posts}} = await response.json();
-    setPosts(posts);
-    return 
-  } catch (error) {
-    console.log(error);
-  }
+  fetch(`https://strangers-things.herokuapp.com/api/${cohortName}/posts`)
+  .then(response => response.json())
+  .then(result => {
+    console.log(result.data.posts);
+    setPosts(result.data.posts)
+  })
+  .catch(console.error);
+
+//   try {
+//     const response = await fetch (`https://strangers-things.herokuapp.com/api/${cohortName}/posts`);
+//     const {data: {posts}} = await response.json();
+//     setPosts(posts);
+//     return 
+//   } catch (error) {
+//     console.log(error);
+//   }
 }
 
