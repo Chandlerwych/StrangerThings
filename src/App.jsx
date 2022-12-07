935852946911cb85a14524e3dd4d548511dc14e6
Brandon Norris <bsn2qg@virginia.edu>
2022-12-07 13:39:32 -0500

    Added Register file

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { getPosts } from "./api/auth";
 import PostsComponent from "./components/PostsComponent";
+import Register from "./components/Register";
 
 
 
 function App() {
  const [posts, setPosts] = useState([]);
- const [username, setUsername] = useState("");
- const [password, setPassword] = useState("");
- const [testPassword, setTestPassword] = useState("");
-
 
 useEffect(() => {
   // Need to add something to dependency array to force reruns
@@ -23,21 +20,9 @@ console.log(testPassword);
 
 
   return ( 
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      // if({password} === {testPassword}){
-      //   // Force into string then compare
-      //   console.log("Great news, I am working");}
-      //   console.log("I am not the same")
-      //   console.log("This is the password: ", password, "This is the testPassword: ", testPassword)
-      
-    }}>
-      {/* e = event */}
-      <input type="text" value={username} placeholder="Username" required onChange={(e)=>{setUsername(e.target.value)}}></input>
-      <input type="password" value={password} placeholder="Password" required minLength="8" onChange={(e)=>{setPassword(e.target.value)}}></input>
-      <input type="password" value={testPassword} placeholder="Confirm Password" required onChange={(e)=>{setTestPassword(e.target.value)}}></input>
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <Register />
+    </div>
     // posts.map(singlePost => {
       //   return (
         //     <PostsComponent key={singlePost._id} singlePost={singlePost} />
